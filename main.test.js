b97b3d3c13e30a9bf7af07c4c642ca32d65e8d20
DANIEL JAMES <daniel.james@codeimmersives.com>
2020-06-02 16:19:59 -0400

    completed test, jest watch passed

--- main.test.js
+++ main.test.js
@@ -2,10 +2,38 @@
  * YOUR CODE GOES HERE THIS TIME! *
  **********************************/
 
+let x = 3;
+let y = 10;
+let squareTheNumber = 4;
+let greeting = 'hello';
+let firstName = 'Daniel';
+let calling = 'ringing';
 
 
+function double() {
+  x = x * 2;
+}
 
- 
+function triple() {
+  y = y * 3;
+}
+
+function square() {
+  squareTheNumber = squareTheNumber * squareTheNumber;
+}
+
+function beingNice() {
+  greeting = 'hello ' +  firstName;
+}
+
+function fullName() {
+  firstName = 'not found';
+}
+
+function phoneCall() {
+  calling = 'call ended';
+}
+
 
 /*********************************************************************************************************************
 
